a8f28483344c2ff8de5b6f5541f3acec9d30b83e
Alex <moldovanalexismihai06@gmail.com>
2023-04-07 14:21:26 +0300

    posts side to be done

--- src/components/home/LeftSide.jsx
+++ src/components/home/LeftSide.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Rocket from '../../images/rocket.png'
 
 const LeftSide = () => {
   return (
-    <div className='posts'>
-      link
+    <div className='leftSide flex flex-col py-10 px-5 h-[100%] bg-gray50 sm:bg-purple15 sm:border-t-2 sm:border-t-gray50'>
+      <div className='options flex flex-col gap-5'>
+        <div className='friends flex items-center gap-3'>
+            <p className="text-textGray sm:text-gray50">Friends</p>
+            <i className="bi bi-people-fill text-pink5 text-lg"></i>
+          </div>
+          <div className='profile flex items-center gap-3'>
+            <p className="text-textGray sm:text-gray50">Your profile</p>
+            <div className='border-2 border-pink5 rounded-full'>
+              <img src='https://media.discordapp.net/attachments/724220064223592541/1092840802721480804/user.jpg?width=662&height=662' alt='profileImage' className="rounded-full w-8 h-8 z-20"/>
+            </div>
+          </div>
+          <div className='settings flex items-center gap-3'>
+            <p className='text-textGray sm:text-gray50'>Settings</p>
+            <i className="bi bi-sliders text-lg text-pink5"></i>
+          </div>
+      </div>
+
+      <div className="logo&name&rocket mt-auto mb-auto ml-auto mr-auto">
+          <div className='flex gap-1 items-center'>
+            <i className="bi bi-circle-fill text-2xl text-pink5"></i>
+              <p className="font-semibold text-lg lg:mr-auto">
+                AMedia app
+                <span className="font-semibold text-pink5"> .</span>
+              </p>
+          </div>
+          <img src={Rocket} alt='blowing rocket' className='w-32 h-32'/>
+      </div>
+       
+
+      <div className='T&C sm:text-gray50'>
+        <p className='text-xs'>I Agree with <span className='text-[#3f9ee3]'>Terms and Conditions</span></p>
+        <p className='text-xs text-center'>© Copyright 2023</p>
+      </div>
+
     </div>
   )
 }
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState,useRef,useEffect } from 'react'
 import LeftSide from './LeftSide'
 
-const Navbar = ({setShader}) => {
+const Navbar = () => {
 
 
   
@@ -27,29 +27,29 @@ const Navbar = ({setShader}) => {
 
   return (
     <div className='bg-purple15'>
-        <div className='px-7 py-5 flex items-center justify-between max-w-[1600px] ml-auto mr-auto'>
-            <i className='bi bi-list text-pink5 text-3xl sm:hidden ' onClick={LeftSideFunction}></i>
+        <div className='px-5 py-3 sm:px-7 sm:py-5 flex items-center justify-between max-w-[1600px] ml-auto mr-auto'>
+            <i className='mr-2 bi bi-list text-pink5 text-3xl sm:hidden' onClick={LeftSideFunction}></i>
             { showLeftSide &&
-                <div className='absolute left-0 top-0 bottom-0 sm:hidden bg-gray50' ref={LeftDiv}>
+                <div className='absolute left-0 top-0 bottom-0 sm:hidden bg-gray50 border-r-2 border-textGray' ref={LeftDiv}>
                     <LeftSide/>
                 </div>
             }
-            <div className='search&logo flex items-center gap-4 lg:flex-row-reverse lg:w-[100%]'>
-                <input className='outline-none px-3 py-2 rounded-full lg:mr-auto lg:ml-auto lg:w-[40%]' type='text' placeholder='@username. . .'/>
-                <div className="logo&name flex gap-1 items-center">
-                <i className="bi bi-circle-fill text-2xl text-pink5"></i>
-                <p className="font-semibold text-lg lg:mr-auto">
-                    AMedia app
-                    <span className="font-semibold text-pink5"> .</span>
-                </p>
+            <div className='search&logo w-[100%] sm:w-[] flex items-center gap-4 lg:flex-row-reverse lg:w-[100%] '>
+                <input className='w-[100%] outline-none px-3 py-1 sm:py-2 rounded-full lg:mr-auto lg:ml-auto lg:w-[40%] max-w-[600px]' type='text' placeholder='@username. . .'/>
+                <div className="hidden xs:flex logo&name gap-1 items-center">
+                  <i className="bi bi-circle-fill text-2xl text-pink5"></i>
+                  <p className="font-semibold text-lg lg:mr-auto whitespace-nowrap">
+                      AMedia app
+                      <span className="font-semibold text-pink5"> .</span>
+                  </p>
                 </div>
             </div>
-            <div className='hidden features sm:flex sm:items-center sm:gap-5'>
+            <div className='hidden features md:flex md:items-center md:gap-5 ml-5'>
                 <div className='hidden md:block'>
                     <i className="bi bi-people-fill text-white text-lg"></i>
                 </div>
                 <div className='profile hover:scale-105 active:scale-95 relative'>
-                    <img src='https://media.discordapp.net/attachments/724220064223592541/1092840802721480804/user.jpg?width=662&height=662' alt='profileImage' className="rounded-full w-8 h-8 z-20"/>
+                    <img src='https://media.discordapp.net/attachments/724220064223592541/1092840802721480804/user.jpg?width=662&height=662' alt='profileImage' className="object-cover rounded-full w-8 h-8 z-20"/>
                     <div className='absolute -bottom-3 -right-2'>
                         <i className="bi bi-caret-down-fill text-pink5"></i>
                     </div>
